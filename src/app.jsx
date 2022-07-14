60284bd0567312989bec5bca01c75b44a5e2c417
Nikita <tr_nikita@mail.ru>
2022-07-15 00:57:10 +0300

    init

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Counter from './components/counter';
+import CountersList from './components/countersList';
+
+
+const App = () => {
+    return (
+        <div>
+            <CountersList/>
+        </div>
+    );
+};
+
+export default App;
